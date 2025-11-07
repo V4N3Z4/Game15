78525cee7a42ae543cc63d72508c2e4f9ac5d7ce
V4N3Z4 <vanessa.zamorano@yh.nackademin.se>
2025-11-07 07:38:30 +0100

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import javax.swing.*;
+
 public class Main{
     public static void main(String[] args){
-        javax.swing.SwingUtilities.invokeLater(Board::new);
+        SwingUtilities.invokeLater(() ->new Board());
     }
 }
