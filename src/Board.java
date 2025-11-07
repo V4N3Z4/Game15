ec1cdf9b2249ae83fe734feffde9fcbb4cde81d1
V4N3Z4 <vanessa.zamorano@yh.nackademin.se>
2025-11-07 09:05:50 +0100

    Board.java

--- src/Board.java
+++ src/Board.java
@@ -14,7 +14,7 @@ public class Board extends JFrame {
         createButtons(panel);
 
         JButton newGameButton = new JButton("New Game");
-        newGameButton.addActionListener(_ -> {
+        newGameButton.addActionListener(e -> {
             controller.Newgame();
             updateButtons();
         });
@@ -30,7 +30,65 @@ public class Board extends JFrame {
                 JButton button = new JButton();
                 buttons[row][col] = button;
                 int r = row, c = col;
-                button.addActionListener(_ ->{
+                button.addActionListener(e ->{
+                    if (controller.moveTile(r,c)){
+                        updateButtons();
+                        if(controller.isGameWon()){
+                            JOptionPane.showMessageDialog(this, "Congratulations, you won!");
+                        }
+                    }
+                });
+                panel.add(button);
+            }
+        }
+        updateButtons();
+    }
+    public  void updateButtons(){
+        for(int row = 0; row < 4; row++){
+            for(int col = 0; col < 4; col++){
+                Tile tile = controller.getTile(row,col);
+                JButton button = buttons[row][col];
+                button.setText(tile.toString());
+                button.setEnabled(!tile.isEmpty());
+            }
+        }
+    }
+
+}
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Board extends JFrame {
+    private final JButton[][] buttons = new JButton[4][4];
+    private final  Controller controller = new Controller();
+
+    public Board(){
+        setTitle("15 Game Board");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        JPanel panel = new JPanel(new GridLayout(4,4));
+        createButtons(panel);
+
+        JButton newGameButton = new JButton("New Game");
+        newGameButton.addActionListener(e -> {
+            controller.Newgame();
+            updateButtons();
+        });
+
+        add(panel,BorderLayout.CENTER);
+        add(newGameButton, BorderLayout.SOUTH);
+        pack();
+        setVisible(true);
+    }
+    private  void createButtons(JPanel panel){
+        for(int row =0; row < 4; row++){
+            for(int col = 0; col < 4; col++){
+                JButton button = new JButton();
+                buttons[row][col] = button;
+                int r = row, c = col;
+                button.addActionListener(e ->{
                     if (controller.moveTile(r,c)){
                         updateButtons();
                         if(controller.isGameWon()){
